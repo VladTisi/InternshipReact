7f6f05404bdced36e86ef6e5fd63b5c7c004e531
Vlad Tisianu <vlad.tisianu@totalsoft.ro>
2022-09-19 14:45:19 +0300

    ,.

--- src/constants/menuConfig.js
+++ src/constants/menuConfig.js
@@ -6,10 +6,10 @@ import GroupIcon from '@material-ui/icons/Group'
 
 const menuItems = [{ icon: <Dashboard />, text: 'NavBar.Dashboard', path: '/dashboard', name: 'Dashboard' },
 {icon: <GroupIcon/>, text: 'NavBar.EmployeesMenu', path: '/employeesmenu', name: 'EmployeesMenu'  },
-{icon: <PermContactCalendar/>, text: 'NavBar.HollidayMenu', path: '/hollidaymenu', name: 'HollidayMenu'  }]
+{icon: <PermContactCalendar/>, text: 'NavBar.HollidayMenu', path: '/hollidaymenu', name: 'HollidayMenu'  },
+{icon:<AddBox />,text:"CreareConcediu",path:"/CreareConcediu",name:"CreareConcediu"}]
 
 
-const menuItems = [{ icon: <Dashboard />, text: 'NavBar.Dashboard', path: '/dashboard', name: 'Dashboard' },{icon:<AddBox />,text:"CreareConcediu",path:"/CreareConcediu",name:"CreareConcediu"}];
 
 
 export default menuItems
