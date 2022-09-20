fbcbd3b6621ccfb3bdf0e4208b59c37e6f1ca258
VladutMielu <vladut.mielu@totalsoft.ro>
2022-09-20 11:34:12 +0300

    ss

--- src/components/hollidaylist/list.js
+++ src/components/hollidaylist/list.js
@@ -1,16 +1,86 @@
 import React from 'react'
 import lista from './dummydata'
+import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next'
+import { makeStyles, useTheme, withStyles } from '@material-ui/core/styles';
 import 'assets/css/hollidaylist.css'
-import { TableContainer,TablePagination,Table, TableBody, TableHead, TableFooter,TableRow, TableCell } from '@material-ui/core';
+import { TableContainer,TablePagination,Table, TableBody, TableHead, TableFooter,TableRow, TableCell, IconButton } from '@material-ui/core';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
 
 
 const cache = createCache({
   key: 'css',
   prepend: true,
 });
+const useStyles1 = makeStyles((theme) => ({
+    root: {
+      flexShrink: 0,
+      marginLeft: theme.spacing(2.5),
+    },
+  }));
+  
+function TablePaginationActions(props) {
+    const classes = useStyles1();
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onPageChange } = props;
+  
+    const handleFirstPageButtonClick = (event) => {
+      onPageChange(event, 0);
+    };
+  
+    const handleBackButtonClick = (event) => {
+      onPageChange(event, page - 1);
+    };
+  
+    const handleNextButtonClick = (event) => {
+      onPageChange(event, page + 1);
+    };
+  
+    const handleLastPageButtonClick = (event) => {
+      onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+  
+    return (
+      <div className={classes.root}>
+        <IconButton
+          onClick={handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="first page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="last page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </div>
+    );
+  }
+  
+  TablePaginationActions.propTypes = {
+    count: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+  };
 export default function ListGetter(){
         const { t } = useTranslation()
         const [page, setPage] = React.useState(0);
@@ -78,27 +148,7 @@ export default function ListGetter(){
                  }}
                 onPageChange={handleChangePage}
                 onRowsPerPageChange={handleChangeRowsPerPage}
-                //ActionsComponent={TablePaginationActions}
-                //component={Box}
-                labelDisplayedRows={({ page }) => {
-                return `Page: ${page}`;
-                }}
-                backIconButtonProps={{
-                    color: "secondary"
-                }}
-                nextIconButtonProps={{ color: "secondary" }}
-                showFirstButton={true}
-                showLastButton={true}
-                labelRowsPerPage={<span>Rows:</span>}
-                sx={{
-                    ".MuiTablePagination-toolbar": {
-                    backgroundColor: "rgba(100,100,100,0.5)"
-                    },
-                    ".MuiTablePagination-selectLabel, .MuiTablePagination-input": {
-                    fontWeight: "bold",
-                    color: "blue"
-                    }
-                }}
+                ActionsComponent={TablePaginationActions}
             />
             </TableFooter>
         </Table>
