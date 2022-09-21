3b6133cdd12b8b5839a718724f17b332218ddf07
VladutMielu <vladut.mielu@totalsoft.ro>
2022-09-21 10:54:45 +0300

    ss

--- src/features/hollidaylist/HollidayList.js
+++ src/features/hollidaylist/HollidayList.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Card,CardHeader } from '@material-ui/core'
 import ListGetter from 'components/hollidaylist/list'
+import { useTranslation } from 'react-i18next'
 
 export default function HollidayList(){
+    const { t }= useTranslation()
     return(
-        <div>
+        <Card className="card-container" style={{paddingBottom:20}}>
+            <CardHeader className="card-header" titleTypographyProps={{variant:'h3' , align:'center',fontStyle:"Calibri" }}
+        title={t('EveryonesHollidays')}/>
             <ListGetter />
-        </div>
+        </Card>
     )
 }
--- src/features/hollidaymenu/HollidayMenu.js
+++ src/features/hollidaymenu/HollidayMenu.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import {Grid} from '@material-ui/core'
+import {Card, CardHeader} from '@material-ui/core'
 import RenderingArrayOfObjects from 'components/hollidaymenu/Container'
+import { useTranslation } from 'react-i18next'
 
 
 
 function HollidayMenu() {
+  const { t }= useTranslation()
   return (
-      <Grid className="Container">
+      <Card className="card-container">
+        <CardHeader titleTypographyProps={{fontSize:20 , align:'center', fontFamily: 'Times New Roman' }}
+        title={t('PersonalHollidays')}/>
         <RenderingArrayOfObjects />
-      </Grid>
+      </Card>
 
     )
   
