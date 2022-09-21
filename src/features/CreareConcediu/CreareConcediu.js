23c767d6b5ae2615e9e42249aff2b6c0065b597b
Vlad Tisianu <vlad.tisianu@totalsoft.ro>
2022-09-21 13:52:20 +0300

    s

--- src/features/CreareConcediu/CreareConcediu.js
+++ src/features/CreareConcediu/CreareConcediu.js
@@ -17,14 +17,13 @@
 //   client.query({ query }).then(response => {
 //     console.log(response.data.person.name);
 //   });
-
 import 'date-fns'
 import Grid from '@material-ui/core/Grid'
+import DateFnsUtils from '@date-io/date-fns'
 import { MuiPickersUtilsProvider, KeyboardTimePicker, KeyboardDatePicker } from '@material-ui/pickers'
-import React, { useEffect, useState } from 'react'
+import DateTimePicker from '../../components/CreareConcediu/DateTimePicker.js'
+import React, { useEffect, useState, useReducer } from 'react'
 import ConcediiDataProvider from './QueriesCC.js'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
 import TextField from '@material-ui/core/TextField'
 import Autocomplete from '@material-ui/lab/Autocomplete'
 import 'assets/css/creareconcediu.css'
@@ -39,77 +38,99 @@ var data2 = [
   { id: 1, name: 'Inloc 2' },
   { id: 2, name: 'Inloc 3' }
 ]
+
 function CreareConcediu() {
-  const [dataInceperii, setDataInceperii] = useState(new Date())
-  const [dataSfarsitului, setDataSfarsitului] = useState(new Date())
+  const initialState = {
+    dataInceperii: new Date(),
+    dataSfarsitului: new Date()
+  }
+  // const [dataInceperii, setDataInceperii] = React.useState(new Date())
+  // const [dataSfarsitului, setDataSfarsitului] = React.useState(new Date())
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  function reducer(state, action) {
+    let newState
+    switch (action.type) {
+      case 'setDataInceperii':
+        newState = { ...state, dataInceperii: action.e }
+        break
+      case 'setDataSfarsitului':
+        newState = { ...state, dataSfarsitului: action.e }
+        // state.dataSfarsitului = newState
+        break
+      default:
+        throw new Error()
+    }
+    return newState
+  }
+
+  const setDataSfarsitului = () => {
+    let x = dispatch('setDataSfarsitului')
+    console.log(x)
+    return x
+  }
+
+  console.log(setDataSfarsitului)
   const [option, setOption] = useState('')
-  useEffect(() => {
-    console.log(dataInceperii)
-  }, [dataInceperii, dataSfarsitului])
+  // useEffect(() => {
+  //   console.log(initialState.dataInceperii)
+  // }, [initialState.dataInceperii, initialState.dataSfarsitului])
 
   return (
     <div className='container22'>
       <div className='title22'>
-        <div className='titleText22'>
-          <header>Pagina Creare Concediu</header>
+        <div className='titleText22'>Pagina Creare Concediu</div>
+      </div>
+      <div className='card'>
+        <importname></importname>
+        <div className='dataInceperii'>
+          {DateTimePicker({
+            label: 'Data Inceperii',
+            value: state.dataInceperii,
+            func: e => dispatch({ type: 'setDataInceperii', e: e })
+          })}
+        </div>
+        <div className='dataSfarsitului'>
+          {DateTimePicker({
+            label: 'Data Sfarsitului',
+            value: state.dataSfarsitului,
+            func: e => dispatch({ type: 'setDataSfarsitului', e: e })
+          })}
+        </div>
+        <div className='tipConcediu'>
+          <Autocomplete
+            options={data}
+            id='cmbTipConcediu'
+            style={{ width: 300 }}
+            getOptionLabel={option => option.name}
+            value={option.id}
+            renderInput={params => <TextField {...params} label='Tip Concediu' />}
+          />
+        </div>
+        <div className='Inlocuitor'>
+          <Autocomplete
+            options={data2}
+            id='cmbInlocuitor'
+            style={{ width: 300 }}
+            getOptionLabel={option => option.name}
+            value={option.id}
+            renderInput={params => <TextField {...params} label='Inlocuitor' />}
+          />
+        </div>
+        <div className='Comentarii'>
+          <div className='TextComentarii'>Comentarii:</div>
+          <div className='TextBoxComentarii'>
+            <TextField></TextField>
+          </div>
+        </div>
+        <div className='ZileRamase'>
+          <div className='ZileRamaseText'>Zile ramase: </div>
+          <div className='ZileRamaseOdihna'>Odihna: </div>
+          <div className='ZileRamaseDeces'>Deces: </div>
+          <div className='ZileRamaseMedical'>Medical: </div>
+          <div className='ZileRamaseNeplatite'>Neplatite: </div>
         </div>
       </div>
-      <body>
-        <div className='card'>
-          <div className='dataInceperii'>
-            {/* Data inceperii:
-            <DatePicker selected={dataInceperii} onChange={date => setDataInceperii(date)} /> */}
-            {/* <KeyboardDatePicker
-              margin='normal'
-              id='date-picker-dialog'
-              label='Date picker dialog'
-              format='MM/dd/yyyy'
-              value={dataInceperii}
-              onChange={setDataInceperii}
-              KeyboardButtonProps={{
-                'aria-label': 'change date'
-              }}
-            /> */}
-          </div>
-          <div className='dataSfarsitului'>
-            Data sfarsitului:
-            <DatePicker selected={dataSfarsitului} onChange={date => setDataSfarsitului(date)} />
-          </div>
-          <div className='tipConcediu'>
-            <Autocomplete
-              options={data}
-              id='cmbTipConcediu'
-              style={{ width: 300 }}
-              getOptionLabel={option => option.name}
-              value={option.id}
-              renderInput={params => <TextField {...params} label='Tip Concediu' />}
-            />
-          </div>
-          <div className='Inlocuitor'>
-            <Autocomplete
-              options={data2}
-              id='cmbInlocuitor'
-              style={{ width: 300 }}
-              getOptionLabel={option => option.name}
-              value={option.id}
-              renderInput={params => <TextField {...params} label='Inlocuitor' />}
-            />
-          </div>
-          <div className='Comentarii'>
-            <div className='TextComentarii'>Comentarii:</div>
-            <div className='TextBoxComentarii'>
-              <TextField></TextField>
-            </div>
-          </div>
-          <div className='ZileRamase'>
-            <div className='ZileRamaseText'>Zile ramase: </div>
-            <div className='ZileRamaseOdihna'>Odihna: </div>
-            <div className='ZileRamaseDeces'>Deces: </div>
-            <div className='ZileRamaseMedical'>Medical: </div>
-            <div className='ZileRamaseNeplatite'>Neplatite: </div>
-          </div>
-        </div>
-      </body>
     </div>
   )
 }
