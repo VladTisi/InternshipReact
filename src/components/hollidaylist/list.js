0e16589a3b9b94e786d4f8b8b6974266e754d106
VladutMielu <vladut.mielu@totalsoft.ro>
2022-09-22 09:30:13 +0300

    ss

--- src/components/hollidaylist/list.js
+++ src/components/hollidaylist/list.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import lista from './dummydata'
 import 'assets/css/hollidaylist.css'
-import { TableContainer,TablePagination,Table, TableFooter} from '@material-ui/core';
+import { TableContainer,Table} from '@material-ui/core';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
-import Actions from 'components/hollidaymenu/Actions';
+import TableFoot from 'components/hollidaymenu/TableFooter';
 import TableHeader from 'components/hollidaymenu/TableHeader';
-import { useTranslation } from 'react-i18next';
 import TableB from 'components/hollidaymenu/TableBody';
 import HLButtons from './hlButtons';
 
@@ -16,41 +15,29 @@ const cache = createCache({
   prepend: true,
 });
 export default function ListGetter(){
-        const {t}= useTranslation()
         const [page, setPage] = React.useState(0);
         const [rowsPerPage, setRowsPerPage] = React.useState(5);
         const [data, setData] = React.useState([]);
         React.useEffect(() => {
             setData(lista.filter(lista=> lista.status=="In Asteptare"));
           }, []);
-          const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0;
+        const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0;
         const handleChangePage = (event, newPage) => {
             setPage(newPage);
-          };
-          const handleChangeRowsPerPage = (event) => {
-            setRowsPerPage(parseInt(event.target.value, 10));
-            setPage(0);
-          };
-          const prop={emptyRows,rowsPerPage,page,data}
-          const prop2={setData,setPage,lista}
+        };
+        const handleChangeRowsPerPage = (event) => {
+          setRowsPerPage(parseInt(event.target.value, 10));
+          setPage(0);
+        };
+        const prop={emptyRows,rowsPerPage,page,data,handleChangeRowsPerPage,handleChangePage}
+        const prop2={setData,setPage,lista}
     return(
         <CacheProvider value={cache}>
         <TableContainer className="space">
         <Table className="tabela">
             <TableHeader/>
             <TableB {...prop}/>
-            <TableFooter className="tblFoot">
-            <TablePagination 
-                rowsPerPageOptions={[]}
-                count={data.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{inputProps: {"aria-label": "rows per page"}}}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-                ActionsComponent={Actions}
-            />
-            </TableFooter>
+            <TableFoot {...prop}/>
         </Table>
         </TableContainer>
         <HLButtons {...prop2}/>
