a295c657f4c39f6c451e877531e1a756aed205e7
VladutMielu <vladut.mielu@totalsoft.ro>
2022-09-22 17:45:24 +0300

    ss

--- src/components/hollidaymenu/Container.js
+++ src/components/hollidaymenu/Container.js
@@ -8,11 +8,17 @@ import TableFoot from './TableFooter';
 import TableHeader from './TableHeader';
 import TableB from './TableBody';
 import HMButtons from './hmButtons';
+import { useReducer } from 'react';
+import reducer from 'features/CreareConcediu/reducerHook';
+
 
 const cache = createCache({
   key: 'css',
   prepend: true,
 });
+
+
+const initialState={lista}
 export default function HollidayM(){
         const [page, setPage] = React.useState(0);
         const [rowsPerPage, setRowsPerPage] = React.useState(5);
@@ -20,10 +26,11 @@ export default function HollidayM(){
         React.useEffect(() => {setData(lista)}, [])
         const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0
         const handleChangePage = (event, newPage) => {setPage(newPage)}
+        const [state, dispatch] = useReducer(reducer, initialState)
         const handleChangeRowsPerPage = (event) => {
             setRowsPerPage(parseInt(event.target.value, 10))
             setPage(0)}
-        const prop={setData,setPage,lista,emptyRows,rowsPerPage,page,data,handleChangeRowsPerPage,handleChangePage}
+        const prop={dispatch,setData,setPage,lista,emptyRows,rowsPerPage,page,data,handleChangeRowsPerPage,handleChangePage}
     return(
         <CacheProvider value={cache}>
         <TableContainer>
