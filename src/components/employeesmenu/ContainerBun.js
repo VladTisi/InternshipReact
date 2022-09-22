3df749119fb8fe8294ea6faed3c67700bb1ac0d7
VladutMielu <vladut.mielu@totalsoft.ro>
2022-09-22 10:54:03 +0300

    ss

--- src/components/employeesmenu/ContainerBun.js
+++ src/components/employeesmenu/ContainerBun.js
@@ -1,100 +1,40 @@
 import React, { useState } from 'react';
 import Head from './TableHeader';
-import {TableBody, Table, TableCell,TableContainer,
-TableFooter,TablePagination,TableRow} from '@material-ui/core';
+import { Table,TableContainer} from '@material-ui/core';
+import TableFoot from 'components/hollidaymenu/TableFooter';
 import lista from "./data1"
-import { useTranslation } from 'react-i18next'
+import TableBBun from './TableBody';
 import 'assets/css/hollidaylist.css' 
 import 'assets/css/SearchBar.css'
-import Actions from 'components/hollidaymenu/PagActions';
-import MappingBun from './MappingBun'
+import SearchBar from './SearchBox';
 
-const rows = lista
+
+
+const data = lista
 
 
 export default function CustomPaginationActionsTable() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
-
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
-
   const[searchTerm,setSearchTerm]= useState("");
+  const prop={rowsPerPage,data,emptyRows,page,searchTerm,handleChangePage,handleChangeRowsPerPage}
+  const searchProp={setSearchTerm}
   return (
     <TableContainer  style={{paddingBottom:20}}>
-      <div className='search'><input
-      type = "text"
-      placeholder="Search..."
-      onChange={(event)=> {
-        setSearchTerm(event.target.value
-          )
-      }}
-      /></div>
+      <SearchBar{...searchProp}/>
       <Table className='tabela' aria-label="custom pagination table">
-              <Head/>
-              <TableBody>
-                  {(rowsPerPage > 0
-
-                    ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-
-                    : rows
-
-                  ).filter((rows)=>{
-                              if(searchTerm == "")
-                              {
-                                return rows
-                              }
-                              else if(JSON.stringify(rows.nume).toLowerCase().includes(searchTerm.toLowerCase()))
-                              {
-                                return rows 
-                              }
-                              
-                            }).map((rows) => (
-
-                    MappingBun(rows)
-
-                  ))}
-
-
-
-                  {emptyRows > 0 && (
-
-                    <TableRow style={{ height: 53 * emptyRows }}>
-
-                      <TableCell colSpan={6} />
-
-                    </TableRow>
-
-                  )}
-
-                  </TableBody>
-                          <TableFooter>
-                            <TableRow>
-                              <TablePagination
-                                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                                colSpan={3}
-                                count={rows.length}
-                                rowsPerPage={rowsPerPage}
-                                page={page}
-                                SelectProps={{
-                                  inputProps: { 'aria-label': 'rows per page' },
-                                  native: true,
-                                }}
-                                onPageChange={handleChangePage}
-                                onRowsPerPageChange={handleChangeRowsPerPage}
-                                ActionsComponent={Actions}
-                              />
-                            </TableRow>
-                          </TableFooter>
-                        </Table>
-                      </TableContainer>
-                    );
+          <Head/>
+          <TableBBun{...prop}/>
+          <TableFoot{...prop}/>
+        </Table>
+      </TableContainer>
+  );
 }
