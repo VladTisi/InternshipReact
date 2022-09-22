c6f1d5d3b56a607c435490739ab96aefdf2dab44
VladutMielu <vladut.mielu@totalsoft.ro>
2022-09-22 10:19:58 +0300

    ss

--- src/components/employeesmenu/ContainerBun.js
+++ src/components/employeesmenu/ContainerBun.js
@@ -1,89 +1,18 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import {TableBody,TableHead, Table, TableCell,TableContainer,
-TableFooter,TablePagination,TableRow,IconButton} from '@material-ui/core';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
+import Head from './TableHeader';
+import {TableBody, Table, TableCell,TableContainer,
+TableFooter,TablePagination,TableRow} from '@material-ui/core';
 import lista from "./data1"
 import { useTranslation } from 'react-i18next'
 import 'assets/css/hollidaylist.css' 
 import 'assets/css/SearchBar.css'
+import Actions from 'components/hollidaymenu/PagActions';
 import MappingBun from './MappingBun'
-
-const useStyles1 = makeStyles((theme) => ({
-  root: {
-    flexShrink: 0,
-    marginLeft: theme.spacing(2.5),
-
-  },
-}));
-
-function TablePaginationActions(props) {
-  const classes = useStyles1();
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onPageChange } = props;
-
-  const handleFirstPageButtonClick = (event) => {
-    onPageChange(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onPageChange(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </div>
-  );
-}
-
-TablePaginationActions.propTypes = {
-  count: PropTypes.number.isRequired,
-  onPageChange: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
 
 const rows = lista
 
 
 export default function CustomPaginationActionsTable() {
-  const { t } = useTranslation()
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
@@ -101,7 +30,7 @@ export default function CustomPaginationActionsTable() {
   const[searchTerm,setSearchTerm]= useState("");
   return (
     
-    <TableContainer  style={{paddingBottom:30}}>
+    <TableContainer  style={{paddingBottom:20}}>
       <div className='search'><input
       type = "text"
       placeholder="Search..."
@@ -112,13 +41,7 @@ export default function CustomPaginationActionsTable() {
       /></div>
       
       <Table className='tabela' aria-label="custom pagination table">
-              <TableHead className='cap'>
-                <TableRow>
-                  <th align="center" >{t('EmployeesMenu.Nume')}</th >
-                  <th align="center">{t('EmployeesMenu.Prenume')}</th>
-                  <th align="center">{t('EmployeesMenu.Functie')}</th>
-                </TableRow>
-              </TableHead>
+              <Head/>
               <TableBody>
 
 {(rowsPerPage > 0
@@ -170,7 +93,7 @@ export default function CustomPaginationActionsTable() {
               }}
               onPageChange={handleChangePage}
               onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
+              ActionsComponent={Actions}
             />
           </TableRow>
         </TableFooter>
