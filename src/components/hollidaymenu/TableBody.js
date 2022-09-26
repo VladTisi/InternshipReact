b4a34517076dd99c8e1626a81709248501e59c06
VladutMielu <vladut.mielu@totalsoft.ro>
2022-09-26 17:14:14 +0300

    ss

--- src/components/hollidaymenu/TableBody.js
+++ src/components/hollidaymenu/TableBody.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 import Mapping from './Mapping'
 
 export default function TableB(props) {
-  const { data, page, rowsPerPage, emptyRows } = props
+  const { lista, page, rowsPerPage, emptyRows } = props
   return (
     <TableBody>
-      {(rowsPerPage > 0 ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : data).map(row => Mapping(row))}
+      {(rowsPerPage > 0 ? lista.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : lista).map(row => Mapping(row))}
       {emptyRows > 0 && (
         <TableRow style={{ height: 55.9 * emptyRows }}>
           <TableCell colSpan={6} />
@@ -17,7 +17,7 @@ export default function TableB(props) {
   )
 }
 TableB.propTypes = {
-  data: PropTypes.object.isRequired,
+  lista: PropTypes.array.isRequired,
   emptyRows: PropTypes.number.isRequired,
   page: PropTypes.number.isRequired,
   rowsPerPage: PropTypes.number.isRequired
