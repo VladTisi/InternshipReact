db5af47b661f9f8dc59b1caf80d80da78351b5f3
Denisa Marica <denisa.marica@totalsoft.ro>
2022-09-26 17:10:58 +0300

    grdttg

--- src/features/ChangePassword/changepassword.js
+++ src/features/ChangePassword/changepassword.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Containers from "../../components/changePassword/containerChangePass.js"
 import { gql, useQuery } from '@apollo/client'
-import { PASSCHANGE_QUERY } from './querryCP.js'
+//import { PASSCHANGE_QUERY } from './querryCP.js'
 
 function SimpleCard() {
-  const [data, loading] = useQuery(PASSCHANGE_QUERY)
-  console.log(data)
+ // const [data, loading] = useQuery(PASSCHANGE_QUERY)
+//  console.log(data)
   return (
     <div className='ContainerUserPage'>
       <Containers />
--- src/features/ChangePassword/querryCP.js
+++ src/features/ChangePassword/querryCP.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { useApolloClient } from '@apollo/client'
-import { useQueryWithErrorHandling } from 'hooks/errorHandling'
-import { gql } from '@apollo/client'
+// import React from 'react'
+// import { useApolloClient } from '@apollo/client'
+// import { useQueryWithErrorHandling } from 'hooks/errorHandling'
+// import { gql } from '@apollo/client'
 
-export const PASSCHANGE_QUERY = gql`
-  query changePasswordDefs {
-    Password {
-      id
-      password
-    }
-  }
-`
+// export const PASSCHANGE_QUERY = gql`
+//   query changePasswordDefs {
+//     Password {
+//       id
+//       password
+//     }
+//   }
+// `
 
-const PasswordProvider = () => {
-  const client = useApolloClient()
-  useQueryWithErrorHandling(PASSCHANGE_QUERY(13), {
-    onCompleted: data => {
-      client.writeQuery({
-        query: PASSCHANGE_QUERY,
-        data: { passwordData: data.passwordData }
-      })
-    }
-  })
-  return <></>
-}
+// const PasswordProvider = () => {
+//   const client = useApolloClient()
+//   useQueryWithErrorHandling(PASSCHANGE_QUERY(13), {
+//     onCompleted: data => {
+//       client.writeQuery({
+//         query: PASSCHANGE_QUERY,
+//         data: { passwordData: data.passwordData }
+//       })
+//     }
+//   })
+//   return <></>
+// }
 
-export default PasswordProvider
+// export default PasswordProvider
