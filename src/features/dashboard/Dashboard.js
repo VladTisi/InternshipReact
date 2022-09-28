17680f696a7803a14125de3442cbdaee935808ce
VladutMielu <vladut.mielu@totalsoft.ro>
2022-09-28 17:19:00 +0300

    s

--- src/features/dashboard/Dashboard.js
+++ src/features/dashboard/Dashboard.js
@@ -5,8 +5,6 @@ import useUserData from 'components/login/useUserData'
 
 function Dashboard() {
   const addToast = useToast()
-  const smt = useUserData()
-  console.log(smt.esteAdmin)
   addToast('Welcome', 'success')
   return (
     <Fragment>
