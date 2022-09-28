2128c38f542aa51c15ed3d10a43a54d05e7909ac
Denisa Marica <denisa.marica@totalsoft.ro>
2022-09-28 14:59:42 +0300

    hh

---
+++ src/features/HirePeople/HirePeople.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card } from '@material-ui/core'
+import { useTranslation } from 'react-i18next'
+import ContainerHirePeople from 'components/HirePeople/Container'
+
+function HirePeople() {
+    
+    const { t }= useTranslation()
+    return(
+        <ContainerHirePeople>
+           
+        </ContainerHirePeople>
+        
+        
+    )
+  }
+  
+  export default HirePeople
